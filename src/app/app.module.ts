4048f3dd504ffdb27868ea64ea927e8f90b0bba1
cega <cgalindez@gmail.com>
2020-11-08 10:40:36 -0300

    1 file modified, 60 added modules auth, books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,82 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { HttpClientModule } from "@angular/common/http";
+import { AuthModule } from './auth';
 
-import { StoreModule } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
-import { StoreRouterConnectingModule, RouterState } from "@ngrx/router-store";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { ROOT_REDUCERS, metaReducers } from './reducers';
 
-import { CoreModule } from "./core";
-
-import { AppComponent } from "./app.component";
+import { CoreModule } from './core';
+import { AppRoutingModule } from './app-routing.module';
+import { UserEffects, RouterEffects } from './core/effects';
+import { AppComponent } from './core/containers';
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CoreModule
+    AuthModule,
+    AppRoutingModule,
+
+    /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.forRoot(ROOT_REDUCERS, {
+      metaReducers,
+      runtimeChecks: {
+        // strictStateImmutability and strictActionImmutability are enabled by default
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+        strictActionWithinNgZone: true,
+        strictActionTypeUniqueness: true,
+      },
+    }),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store.
+     */
+    StoreRouterConnectingModule.forRoot(),
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Book Store App',
+
+      // In a production build you would want to disable the Store Devtools
+      // logOnly: environment.production,
+    }),
+
+    /**
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://ngrx.io/guide/effects#registering-root-effects
+     */
+    EffectsModule.forRoot([UserEffects, RouterEffects]),
+    CoreModule,
   ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
---
+++ src/app/auth/containers/login-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Credentials } from '@example-app/auth/models';
+import * as fromAuth from '@example-app/auth/reducers';
+import { LoginPageActions } from '@example-app/auth/actions';
+
+@Component({
+  selector: 'bc-login-page',
+  template: `
+    <bc-login-form
+      (submitted)="onSubmit($event)"
+      [pending]="pending$ | async"
+      [errorMessage]="error$ | async"
+    >
+    </bc-login-form>
+  `,
+  styles: [],
+})
+export class LoginPageComponent implements OnInit {
+  pending$ = this.store.select(fromAuth.selectLoginPagePending);
+  error$ = this.store.select(fromAuth.selectLoginPageError);
+
+  constructor(private store: Store<fromAuth.State>) {}
+
+  ngOnInit() {}
+
+  onSubmit(credentials: Credentials) {
+    this.store.dispatch(LoginPageActions.login({ credentials }));
+  }
+}
